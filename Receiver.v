bbd054f5ae1b598295a821c2c2e386eab88266d2
Omar <ochavirar@outlool.com>
2022-04-25 21:57:21 -0500

    Queso

--- Receiver.v
+++ Receiver.v
@@ -19,26 +19,61 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module Receiver(
-    input clk,
+    input ps2c,
     input Rx,
-	 output clock
+	 output [7:0]Data
     );
 	 
-	 reg [0:7] data;
-	 reg [11:0] conteo;
-	 reg Frecuencia1;
-	 
-	 always@(posedge clk)
-		 begin
-			conteo <= conteo +1;
-							
-			if(conteo == (2_083))
-				begin
-					Frecuencia1 <= !Frecuencia1;
-					conteo <= 0;
-				end
-		 end
-
+	 reg [7:0] code;
+	 reg [3:0] estado;
+		 
+	 assign Data = code;
+		 
+	 always @(posedge ps2c)
+	 begin 
+		case(estado)
+		0: begin
+			estado <= 1;
+			code <= 0;
+		end
+		1: begin
+			code[7] <= Rx;
+			estado <= 2;
+		end
+		2: begin
+			code[6] <= Rx;
+			estado <= 3;
+		end
+		3: begin
+			code[5] <= Rx;
+			estado <= 4;
+		end
+		4: begin
+			code[4] <= Rx;
+			estado <= 5;
+		end
+		5: begin
+			code[3] <= Rx;
+			estado <= 6;
+		end
+		6: begin
+			code[2] <= Rx;
+			estado <= 7;
+		end
+		7: begin
+			code[1] <= Rx;
+			estado <= 8;
+		end
+		8: begin
+			code[0] <= Rx;
+			estado <= 9;
+		end
+		9:
+			estado <= 0;
+		default:
+			estado <= 0;
+		endcase
+	 end
 
 endmodule
 
--- Top.v
+++ Top.v
@@ -19,7 +19,60 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module Top(
+		input PS2C,
+		input PS2D,
+		output [7:0] DATA
     );
 
+	 reg [7:0]code;
+	 reg [3:0] estado;
+
+	 always @(negedge PS2C)
+	 begin 
+		case(estado)
+		0: begin
+			estado <= 1;
+			code <= 0;
+		end
+		1: begin
+			code[7] <= PS2D;
+			estado <= 2;
+		end
+		2: begin
+			code[6] <= PS2D;
+			estado <= 3;
+		end
+		3: begin
+			code[5] <= PS2D;
+			estado <= 4;
+		end
+		4: begin
+			code[4] <= PS2D;
+			estado <= 5;
+		end
+		5: begin
+			code[3] <= PS2D;
+			estado <= 6;
+		end
+		6: begin
+			code[2] <= PS2D;
+			estado <= 7;
+		end
+		7: begin
+			code[1] <= PS2D;
+			estado <= 8;
+		end
+		8: begin
+			code[0] <= PS2D;
+			estado <= 9;
+		end
+		9:
+			estado <= 0;
+		default:
+			estado <= 0;
+		endcase
+	 end
+
+	assign DATA = code;
 
 endmodule
